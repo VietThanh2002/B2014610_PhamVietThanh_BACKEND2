c954d7d95c7d0152abe11cf5d7b9aeea4d69c614
VietThanh2002 <114978891+VietThanh2002@users.noreply.github.com>
2023-09-09 22:17:15 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express');
+const cors = require('cors');
+const contactRoute = require( "./app/routes/contact.route" );
 
 const app = express();
 
 app.use( cors());
 app.use( express.json());
 
-app.get( '/', ( req, res )=>{ 
+app.get("/", ( req, res )=>{ 
     res.json( {
         message: "Welcome to contract book application."});
 });
 
+app.use( "/api/contacts", contactRoute );
 module.exports = app;
